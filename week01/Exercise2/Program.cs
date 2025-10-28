9c9c726ca3f1f054c3d4fada4f32c9e75e40499f
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-10-28 17:49:48 -0400

    Exercise 2 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the grade percent? ");
+        string gradePecent = Console.ReadLine();
+        int gradeNumber = int.Parse(gradePecent);
+
+        string letter = "";
+        // string letteB = "B";
+        // string letteC = "C";
+        // string letteD = "D";
+        // string letteF = "F";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"You have earned the grade {letter}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! You did not pass the course, consider retaking it.");
+        }   
+    
     }
 }
