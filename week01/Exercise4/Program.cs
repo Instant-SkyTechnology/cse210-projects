9e96a79178efd134af17e705ff8168a8ad16ae0c
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-10-29 13:56:15 -0400

    Exercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> Numbers = new List<int>();
+        Numbers.Sort();
+
+        while (true) 
+        {
+            Console.Write("Enter number: ");
+            int nuM = int.Parse(Console.ReadLine());
+
+            if (nuM == 0)
+            {
+                break;
+            }
+            Numbers.Add(nuM);
+        }
+        Console.WriteLine();
+        Console.WriteLine($"You entered {Numbers.Count} numbers.");
+        if (Numbers.Count > 0)
+        {
+            Console.WriteLine($"The sum is: {Numbers.Sum()}");
+            Console.WriteLine($"The average is: {Numbers.Average():F2}");
+            Console.WriteLine($"The largest number is: {Numbers.Max():F2}");
+            Console.WriteLine($"The smallest positive number is: {Numbers.Min():F2}");
+
+            foreach (int num in Numbers)
+            {
+                Console.WriteLine(num);
+            }
+            
+            Console.WriteLine();
+        }
     }
 }
