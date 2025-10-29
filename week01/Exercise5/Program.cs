955a12c78237ab78b999922f42b63fe1ffdf30af
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-10-29 16:48:29 -0400

    Exercise 5 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string User = PromptUserName();
+
+        int nuM = PromptUserNumber();
+
+        int squarE = SquareNumber(nuM);
+
+        DisplayResult(User, squarE);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string UserName = Console.ReadLine();
+        return UserName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int nuMber = int.Parse(Console.ReadLine());
+        return nuMber;
+    }
+
+    static int SquareNumber(int nuMber)
+    {
+        int square = nuMber * nuMber;
+        return square;
+    }
+
+    static void DisplayResult(string UserName, int square)
+    {
+        Console.WriteLine($"{UserName}, the square of your number is {square}");
+    }
+
 }
