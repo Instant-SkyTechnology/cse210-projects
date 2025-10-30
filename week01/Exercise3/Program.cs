0ec63c9914e4c1ddf84a33334bb00fa36c805376
Instant-SkyTechnology <stevekalala@yahoo.fr>
2025-10-30 09:49:00 -0400

    Exercise 3 modified

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,18 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a random number generator
+        // Random number generator
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
 
         int guess = -1; // Initialize guess with a value that will not equal the magic number
 
+        Console.WriteLine();
         Console.WriteLine("I'm thinking of a number between 1 and 100...");
+        List<int> GuessCount = new List<int>();
 
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
+            GuessCount.Add(guess);
 
             if (guess < magicNumber)
             {
@@ -25,10 +28,25 @@ class Program
             {
                 Console.WriteLine("Lower");
             }
-            else
+            else if (guess == magicNumber)
             {
+                Console.WriteLine();
                 Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {GuessCount.Count()} guesses.");
+
+                Console.Write("Would you like to continue (Yes/No): ");
+                string ContinueGame = Console.ReadLine().Trim().ToLower();
+                if (ContinueGame == "yes" || ContinueGame == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for playing. Goodby!");
+                    Console.WriteLine();
+                    break;
+                }
             }
-        }
+        }    
     }
 }
